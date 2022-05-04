acbc8814e995af6546dff4288b76754ad8b5380c
Malik8008 <tu60prbs1@code.edu.az>
2022-05-04 12:20:16 +0400

    added tasks

---
+++ js/style.js
@@ -0,0 +1,46 @@
+// 1.  Her hansı bir stringin içərisindəki yazıları söz sırası və hərf sırası daxil olmaqla tərs yazdıran bir function yazın
+
+// let word="Salam dostdar"
+// let freeword=""
+// for (let index = word.length-1; index >=0; index--) {
+//     freeword += word[index];   
+// }
+
+// console.log(freeword);
+
+
+
+// 6. Verilmis M ededini verilmis N quvvete yukselden function
+
+// function kyubbet(M,N){
+//     console.log(M**N);
+// }
+
+// kyubbet(5,2);
+
+
+// 7. Verilmis arrayin icerisinde deyeri 45 olan elementin indexini tapan function
+
+// function search(arr){
+//     console.log(arr.indexOf(45));
+// }
+// let arr=[13,45,34,56,6];
+
+// search(arr);
+
+
+
+// 7. Verilmis arrayin icerisinde deyeri 45 olan elementin indexini tapan function
+// function change(num1,num2){
+//      numcopy = num1;
+//      count = num2;
+//            while (numcopy != 0) 
+//            {
+//                numcopy /= 10;
+//                num2 *= 10;
+//            }
+//            let result=num1+num2;
+//            console.log(result);
+// }
+
+// change(123,4);
